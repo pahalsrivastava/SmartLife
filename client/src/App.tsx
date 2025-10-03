912ab1c1a9ffe0b8541dd3614ae2f3a7ef403b29
pahalsrivastava <srivastavapahal5@gmail.com>
2025-10-03 15:38:58 +0530

    fix(client) : made basic changes in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Dashboard from './pages/Dashboard';
 import Habits from './pages/Habits';
-import Expenses from './pages/expenses';
+import Expenses from './pages/Expenses';
 import Reports from './pages/Reports';
 import Settings from './pages/Settings';
 
