95c9e4363cc17448e186297ba403204b62b19961
pahalsrivastava <srivastavapahal5@gmail.com>
2025-10-14 13:00:25 +0530

    fix: revert back to original url

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -25,7 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <ClerkProvider
         publishableKey={clerkPublishableKey}
         afterSignInUrl="/dashboard"
-        afterSignUpUrl="/dashboard"
+        afterSignUpUrl="/"
         afterSignOutUrl="/"
       >
         {AppRoot}
